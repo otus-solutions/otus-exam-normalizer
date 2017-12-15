c8e5e7a001d4f207318daa592a06e4e37a98a495
Tiago Matana <tiago.matana@gmail.com>
2017-12-15 10:53:09 -0200

    OEN-1 states created

--- app/deploy/state/state-configuration.js
+++ app/deploy/state/state-configuration.js
@@ -18,18 +18,30 @@
     '$urlRouterProvider',
     '$stateProvider',
     'normalizerjs.deploy.HomeStateProvider',
-    'normalizerjs.deploy.SaoPauloStateProvider'
+    'normalizerjs.deploy.SaoPauloStateProvider',
+    'normalizerjs.deploy.RioGrandeDoSulStateProvider',
+    'normalizerjs.deploy.MinasGeraisStateProvider',
+    'normalizerjs.deploy.EspiritoSantoStateProvider',
+    'normalizerjs.deploy.BahiaStateProvider',
+    'normalizerjs.deploy.RioDeJaneiroStateProvider'
   ];
 
-  function Configuration($urlRouterProvider, $stateProvider,HomeStateProvider,SaoPauloStateProvider) {
+  function Configuration($urlRouterProvider, $stateProvider,
+    HomeStateProvider,
+    SaoPauloStateProvider,
+    RioGrandeDoSulStateProvider,
+    MinasGeraisStateProvider,
+    EspiritoSantoStateProvider,
+    BahiaStateProvider,
+    RioDeJaneiroStateProvider) {
     //TODO Desenvolver os states de cada conversor
     $stateProvider.state(SaoPauloStateProvider.state);
     $stateProvider.state(HomeStateProvider.state);
-    // $stateProvider.state(RioGrandeDoSulStateProvider.state);
-    // $stateProvider.state(MinasGeraisStateProvider.state);
-    // $stateProvider.state(EspiritoSantoStateProvider.state);
-    // $stateProvider.state(BahiaStateProvider.state);
-    // $stateProvider.state(RioDeJaneiroStateProvider.state);
+    $stateProvider.state(RioGrandeDoSulStateProvider.state);
+    $stateProvider.state(MinasGeraisStateProvider.state);
+    $stateProvider.state(EspiritoSantoStateProvider.state);
+    $stateProvider.state(BahiaStateProvider.state);
+    $stateProvider.state(RioDeJaneiroStateProvider.state);
 
     /* Default state (route) */
     $urlRouterProvider.otherwise(HomeStateProvider.state.url);
