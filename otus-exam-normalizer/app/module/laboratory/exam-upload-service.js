b938d4f729f188a05dceea2c9a6161d972336221
emanoelvianna <emanoel.vianna@outlook.com>
2018-02-20 16:20:50 -0300

    OEN-2 - parcial, inicio da comunicação com o model

--- otus-exam-normalizer/app/module/laboratory/exam-upload-service.js
+++ otus-exam-normalizer/app/module/laboratory/exam-upload-service.js
@@ -6,27 +6,71 @@
     .service('normalizerjs.module.laboratory.ExamUploadService', service);
 
   service.$inject = [
-    'otusjs.laboratory.exam.sending.ExamLotService'
+    'otusjs.laboratory.exam.sending.ExamSendingFactory',
+    'otusjs.laboratory.exam.sending.Exam',
+    'otusjs.laboratory.exam.sending.ExamLot',
+    'otusjs.laboratory.exam.sending.ExamObservation',
+    'otusjs.laboratory.exam.sending.ExamResults'
   ];
 
-  function service(ExamLotService) {
+  function service(ExamSendingFactory, Exam, ExamLot, ExamObservation, ExamResults) {
     var self = this;
 
     /* Public methods */
-    self.createExamSending = createExamSending;
-    self.loadExamSendingFromJson = loadExamSendingFromJson;
-    self.getExamList = getExamList;
+    self.fileStructureToModel = fileStructureToModel;
 
-    function createExamSending() {
-      return ExamLotService.createExamSending();
+    function fileStructureToModel(fileStructure) {
+      var examSending = ExamSendingFactory.create();
+      examSending.examLot.objectType = "ExamLot";
+      examSending.examLot.fileName = fileStructure.fileName;
+      examSending.examLot.realizationDate = new Date();
+      examSending.examLot.resultsQuantity = 0;
+      examSending.examLot.fieldCenter = {
+        acronym: fileStructure.getFieldCenter().acronym
+      };
+
+      fileStructure.rows.forEach(function (row) {
+        if (row.isResult || row.isNewExam || row.isExamObservation || row.isResultObservation) {
+          if (row.isNewExam) {
+            var newExam = Exam.create();
+            newExam.name = row.examName;
+            examSending.exams.push(newExam);
+          }
+          if (row.isResult && examSending.exams && examSending.exams.length) {
+            var newResult = ExamResults.create();
+            newResult.examName = row.examName;
+            newResult.aliquotCode = row.aliquot;
+            //TODO: verificar se é realmente essa data que deve ser obtida!
+            newResult.releaseDate = row.releaseDate;
+            newResult.resultName = row.label;
+            newResult.value = row.result;
+            examSending.exams[examSending.exams.length - 1].examResults.push(
+              newResult
+            )
+          }
+          if (row.isExamObservation && examSending.exams && examSending.exams.length) {
+            var observation = ExamObservation.create();
+            //observation.name //Não sabemos o que vai aqui
+            observation.value = row.observation;
+            examSending.exams[examSending.exams.length - 1].observations.push(observation);
+          }
+          if (row.isResultObservation
+            && examSending.exams && examSending.exams.length
+            && examSending.exams[examSending.exams.length - 1].examResults
+            && examSending.exams[examSending.exams.length - 1].examResults.length
+          ) {
+            var lastExamIndex = examSending.exams.length - 1;
+            var lastResultIndex = examSending.exams[lastExamIndex].examResults.length - 1;
+            var observation = ExamObservation.create();
+            //observation.name //Não sabemos o que vai aqui
+            observation.value = row.observation;
+            examSending.exams[lastExamIndex].examResults[lastResultIndex].observations.push(observation);
+          }
+        }
+      });
+      examSending.examLot.resultsQuantity = examSending.getExamList().length;
+      console.log(examSending);
     }
 
-    function loadExamSendingFromJson(examResultLot, examResults) {
-      return ExamLotService.buildExamSendingFromJson(examResultLot, examResults);
-    }
-
-    function getExamList() {
-      return ExamLotService.getExamList();
-    }
   }
 }());
--- otus-exam-normalizer/app/ux-component/exam-upload-button/exam-upload-button-component.js
+++ otus-exam-normalizer/app/ux-component/exam-upload-button/exam-upload-button-component.js
@@ -28,77 +28,40 @@
 
     var self = this;
     var timeShowMsg = 4000;
-    var fr = new FileReader();
 
-    self.$onInit = onInit;
-    self.fileUpload = fileUpload;
-    self.upload = self.uploadFile || upload;
-    self.validate = self.validateFile || _validateFileToUpload;
-
-    function onInit() {
-      fr.onload = receivedText;
-      self.fileData = {};
-      self.fileData.examResultLot = {};
-      self.fileData.examResultLot.fieldCenter = {};
-
-      self.input = $($element[0].querySelector('#fileInput'));
-      self.input.on('change', function (e) {
-        console.log(e.target.result)
-        console.log(e.target.files[0])
-
-        _convertToWorkbook(e.target.files[0], function(workbook) {
-          console.log(workbook);
-          var rowsArray = XLSX.utils.sheet_to_json(
-            workbook.Sheets[workbook.SheetNames[0]],
-            {
-              header: 1,
-              dateNF: "dd/MM/yy hh:mm",
-              defval: "",
-              raw: false
-            }
-          )
-          console.log("rowsArray", rowsArray);
-          var fileStructure = FileStructureFactory.create();
-          fileStructure.setFieldCenter({acronym: "SP"})
-          fileStructure.createRowsWithSheet(rowsArray).then(
-            console.log(fileStructure)
-          );
-        })
-        self.upload(e.target.files);
-        // if (_validateFileToUpload(e.target.files[0])) {
-        //   fr.readAsText(e.target.files[0]);
-        // }
-      });
-    }
+    self.upload = upload;
 
     $scope.$watch('file', function () {
       self.upload($scope.file);
     });
 
-    function fileUpload(file) {
+    function upload(file) {
       if (file) {
         if (!file.$error) {
-          var progressPercentage;
-          var reader = new FileReader();
-          ExamUploadService.createExamSending();
 
-          reader.onload = function () {
-            //TODO: utilizar o conversor de arquivo aqui, aqui também deve ser chamado o model!
+          //TODO: Chamar o serviço que conversa com o model!
 
-            var text = reader.result;
-            console.log(text);
-          };
-          reader.readAsText($scope.file);
+          _convertToWorkbook(file, function (workbook) {
+            console.log(workbook);
+            var rowsArray = XLSX.utils.sheet_to_json(
+              workbook.Sheets[workbook.SheetNames[0]],
+              {
+                header: 1,
+                dateNF: "dd/MM/yy hh:mm",
+                defval: "",
+                raw: false
+              }
+            )
+            console.log("rowsArray", rowsArray);
+            var fileStructure = FileStructureFactory.create();
+            fileStructure.setFieldCenter({ acronym: "SP" })
+            fileStructure.createRowsWithSheet(rowsArray).then(function () {
+              console.log(fileStructure);
+              ExamUploadService.fileStructureToModel(fileStructure);
+            });
+          });
         }
       }
-
-      //TODO: calcular o processamento de conversão
-      var loaded = 10;
-      var total = 50;
-      progressPercentage = parseInt(100.0 * loaded / total);
-      self.progress = progressPercentage;
-      self.progress = 100;
-      console.log(self.progress);
     }
 
     function _convertToWorkbook(file, callback) {
@@ -113,63 +76,5 @@
       reader.readAsBinaryString(file);
     }
 
-    function upload() {
-      self.input.click();
-    }
-
-    function _validateFileToUpload(file) {
-      if (_typeIsValid(file.type)) {
-        return true;
-      } else {
-        _toastError();
-      }
-    }
-
-    function _typeIsValid(type) {
-      return type === "application/json";
-    }
-
-    function receivedText(e) {
-      var lines = e.target.result;
-      console.log(lines)
-
-      if (1 === 2) {
-        if (!_fileIsEmpty(lines)) {
-          self.fileData.examResults = JSON.parse(lines);
-          ProjectContextService.setFileStructure(self.fileData);
-          self.action = ProjectContextService.setExamSendingAction('upload');
-          ApplicationStateService.activateExamResultsVisualizer();
-        } else {
-          self.input[0].value = '';
-          _toastEmptyFile();
-        }
-      }
-    }
-
-    function _fileIsEmpty(lines) {
-      if (!lines) {
-        return true;
-      } else if (!JSON.parse(lines).length) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    function _toastEmptyFile() {
-      $mdToast.show(
-        $mdToast.simple()
-          .textContent('O arquivo está vazio')
-          .hideDelay(timeShowMsg)
-      );
-    }
-
-    function _toastError() {
-      $mdToast.show(
-        $mdToast.simple()
-          .textContent('Arquivo inválido')
-          .hideDelay(timeShowMsg)
-      );
-    }
   }
 }());
