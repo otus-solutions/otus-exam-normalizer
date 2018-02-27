aff4ffae2dd9b74214bd4510d203d2263483a599
Allister Terhorst Ramos <allistertr@hotmail.com>
2018-02-27 17:17:06 -0300

    OEN-2 - Conclusão de conversão

--- otus-exam-normalizer/tests/unit/app/converter/template/template-service-spec.js
+++ otus-exam-normalizer/tests/unit/app/converter/template/template-service-spec.js
@@ -137,13 +137,299 @@ describe('TemplateService service', function () {
       mockLastResult();
       mockRowFilled();
     });
-    it('the aliquot should be equal to the last result', function () {
-      let returned = service.contains("aliquot", Mock.RowFilled, "");
+    it('(1) the return should be equal to true', function () {
+      let returned = service.contains("aliquot", Mock.RowFilled, ["3"]);
       expect(returned).toEqual(true);
     });
-    it('the examCode examCode not be equal to the last result', function () {
-      let returned = service.contains("examCode", Mock.RowFilled, "");
+    it('(2) the return should be equal to false', function () {
+      let returned = service.contains("aliquot", Mock.RowFilled, ["a"]);
       expect(returned).toEqual(false);
+    });
+  });
+
+  describe('notContains method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+    });
+    it('(1) the return should be equal to false', function () {
+      let returned = service.notContains("aliquot", Mock.RowFilled, ["3"]);
+      expect(returned).toEqual(false);
+    });
+    it('(2) the return should be equal to true', function () {
+      let returned = service.notContains("aliquot", Mock.RowFilled, ["a"]);
+      expect(returned).toEqual(true);
+    });
+  });
+
+  describe('exists method ', function () {
+    it('(1) the return should be equal to false', function () {
+      let returned = service.exists(undefined);
+      expect(returned).toEqual(false);
+    });
+    it('(2) the return should be equal to true', function () {
+      let returned = service.exists("");
+      expect(returned).toEqual(true);
+    });
+    it('(3) the return should be equal to true', function () {
+      let returned = service.exists(0);
+      expect(returned).toEqual(true);
+    });
+    it('(4) the return should be equal to true', function () {
+      let returned = service.exists(false);
+      expect(returned).toEqual(true);
+    });
+  });
+
+  describe('isEmpty method ', function () {
+    it('(1) the return should be equal to true', function () {
+      let returned = service.isEmpty(undefined);
+      expect(returned).toEqual(true);
+    });
+    it('(2) the return should be equal to true', function () {
+      let returned = service.isEmpty("");
+      expect(returned).toEqual(true);
+    });
+    it('(3) the return should be equal to false', function () {
+      let returned = service.isEmpty(0);
+      expect(returned).toEqual(false);
+    });
+    it('(4) the return should be equal to false', function () {
+      let returned = service.isEmpty(false);
+      expect(returned).toEqual(false);
+    });
+  });
+
+  describe('dateValueToISOString method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+    });
+    it('(1) the return should be equal to "2017-05-26T15:11:00.000Z"', function () {
+      let returned = service.dateValueToISOString("26/05/17 12:11", Mock.Template.formatType.date);
+      expect(returned).toEqual("2017-05-26T15:11:00.000Z");
+    });
+    it('(2) the return should be equal to "2017-05-26T15:11:00.000Z"', function () {
+      let returned = service.dateValueToISOString(
+        "m:11, h:12, a:2017, M:5, d:26",
+        {
+          minutes: { start: 2, length: 2 },
+          hours: { start: 8, length: 2 },
+          year: { start: 14, length: 4 },
+          month: { start: 22, length: 1 },
+          day: { start: 27, length: 2 }
+        }
+      );
+      expect(returned).toEqual("2017-05-26T15:11:00.000Z");
+    });
+  });
+
+  describe('getFieldValue method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+    });
+    it('(1) the return should be equal to "363004755"', function () {
+      let returned = service.getFieldValue(
+        Test.utils.data.Fields[0],
+        Test.utils.data.sheet[4],
+        Mock.Template
+      );
+      expect(returned).toEqual("363004755");
+    });
+    it('(2) the return should be equal to "111100"', function () {
+      let returned = service.getFieldValue(
+        Test.utils.data.Fields[1],
+        Test.utils.data.sheet[4],
+        Mock.Template
+      );
+      expect(returned).toEqual("111100");
+    });
+  });
+
+  describe('getObservation method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+    });
+    it('(1) the return should be equal to "363004755"', function () {
+      let returned = service.getObservation(
+        {
+          observationFromTheField: "aliquot"
+        },
+        Mock.RowFilled,
+        Mock.LastResult
+      );
+      expect(returned).toEqual("363004755");
+    });
+    it('(2) the return should be equal to "CREATININA - URINA AMOSTRA ISOLADA"', function () {
+      let returned = service.getObservation(
+        {
+          getObservation: (row, lastResult) => lastResult.examName
+        },
+        Mock.RowFilled,
+        Mock.LastResult
+      );
+      expect(returned).toEqual("CREATININA - URINA AMOSTRA ISOLADA");
+    });
+  });
+
+  describe('validateFunctionRules method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+    });
+    it('(1) the return should be equal to true', function () {
+      let returned = service.validateFunctionRules(
+        {
+          otherValidation: (row, lastResult) => row.aliquot === lastResult.aliquot
+        },
+        Mock.RowFilled,
+        Mock.LastResult
+      );
+      expect(returned).toEqual(true);
+    });
+    it('(2) the return should be equal to false', function () {
+      let returned = service.validateFunctionRules(
+        {},
+        Mock.RowFilled,
+        Mock.LastResult
+      );
+      expect(returned).toEqual(false);
+    });
+  });
+
+  describe('validateSimpleRules method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+    });
+    it('(1) the return should be equal to true', function () {
+      let returned = service.validateSimpleRules(
+        Test.utils.data.Fields[0],
+        {
+          isEmpty: false,
+          equalLastResult: true,
+          contains: ['3'],
+          notContains: ['a']
+        },
+        Mock.RowFilled,
+        Mock.LastResult
+      );
+      expect(returned).toEqual(true);
+    });
+  });
+
+  describe('validateFieldsRule method ', function () {
+    var fields = {};
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+      service.validateFieldsRule(Mock.RowFilled, Mock.LastResult);
+      Mock.RowFilled.fields.forEach(field => {
+        fields[field.name] = field;
+      });
+    });
+    it('the aliquot.resultRuleReturn should be equal to true', function () {
+      expect(fields.aliquot.resultRuleReturn).toEqual(true);
+    });
+    it('the examCode.resultRuleReturn should be equal to true', function () {
+      expect(fields.examCode.resultRuleReturn).toEqual(true);
+    });
+    it('the examName.resultRuleReturn should be equal to true', function () {
+      expect(fields.examName.resultRuleReturn).toEqual(true);
+    });
+    it('the label.resultRuleReturn should be equal to true', function () {
+      expect(fields.label.resultRuleReturn).toEqual(true);
+    });
+    it('the aliquot.examObservationRuleReturn should be undefined', function () {
+      expect(fields.aliquot.examObservationRuleReturn).toEqual(undefined);
+    });
+    it('the examCode.examObservationRuleReturn should be undefined', function () {
+      expect(fields.examCode.examObservationRuleReturn).toEqual(undefined);
+    });
+    it('the examName.examObservationRuleReturn should be undefined', function () {
+      expect(fields.examName.examObservationRuleReturn).toEqual(undefined);
+    });
+    it('the label.examObservationRuleReturn should be undefined', function () {
+      expect(fields.label.examObservationRuleReturn).toEqual(undefined);
+    });
+  });
+
+  describe('validateRow method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+      service.validateFieldsRule(Mock.RowFilled, Mock.LastResult);
+      service.validateRow(Mock.RowFilled, Mock.LastResult, Mock.Template);
+    });
+    it('the isResult should be equal to true', function () {
+      expect(Mock.RowFilled.isResult).toEqual(true);
+    });
+    it('the isNewExam should be equal to true', function () {
+      expect(Mock.RowFilled.isNewExam).toEqual(true);
+    });
+    it('the isExamObservation should be equal to false', function () {
+      expect(Mock.RowFilled.isExamObservation).toEqual(false);
+    });
+    it('the isResultObservation should be equal to false', function () {
+      expect(Mock.RowFilled.isResultObservation).toEqual(false);
+    });
+  });
+
+  describe('createRow method ', function () {
+    beforeEach(function () {
+      mockTemplate();
+      mockLastResult();
+      mockRowFilled();
+      mockRowCreated();
+    });
+    it('the isResult should be equal to true', function () {
+      expect(Mock.RowCreated.isResult).toEqual(true);
+    });
+    it('the isNewExam should be equal to true', function () {
+      expect(Mock.RowCreated.isNewExam).toEqual(true);
+    });
+    it('the isExamObservation should be equal to false', function () {
+      expect(Mock.RowCreated.isExamObservation).toEqual(false);
+    });
+    it('the isResultObservation should be equal to false', function () {
+      expect(Mock.RowCreated.isResultObservation).toEqual(false);
+    });
+    it('the registrationCode should be equal to C0000942782', function () {
+      expect(Mock.RowCreated.registrationCode).toEqual("C0000942782");
+    });
+    it('the aliquot should be equal to 363004755', function () {
+      expect(Mock.RowCreated.aliquot).toEqual("363004755");
+    });
+    it('the solicitationNumber should be equal to 1118947', function () {
+      expect(Mock.RowCreated.solicitationNumber).toEqual("1118947");
+    });
+    it('the orderNumber should be equal to 7839104', function () {
+      expect(Mock.RowCreated.orderNumber).toEqual("7839104");
+    });
+    it('the itemNumber should be equal to 4040767', function () {
+      expect(Mock.RowCreated.itemNumber).toEqual("4040767");
+    });
+    it('the examCode should be equal to 111100', function () {
+      expect(Mock.RowCreated.examCode).toEqual("111100");
+    });
+    it('the examName should be equal to "SÓDIO - URINA AMOSTRA ISOLADA"', function () {
+      expect(Mock.RowCreated.examName).toEqual("SÓDIO - URINA AMOSTRA ISOLADA");
+    });
+    it('the label should be equal to "SÓDIO...................................:"', function () {
+      expect(Mock.RowCreated.label).toEqual("SÓDIO...................................:");
+    });
+    it('the result should be equal to 148', function () {
+      expect(Mock.RowCreated.result).toEqual("148");
+    });
+    it('the releaseDate should be equal to "26/05/17 12:11"', function () {
+      let date = service.dateValueToISOString("26/05/17 12:11", Mock.Template.formatType.date);
+      expect(Mock.RowCreated.releaseDate).toEqual(date);
     });
   });
 
@@ -173,8 +459,8 @@ describe('TemplateService service', function () {
   }
 
   function mockRowFilled() {
-    if(!Mock.Template) mockTemplate();
-    if(!Mock.LastResult) mockLastResult();
+    if (!Mock.Template) mockTemplate();
+    if (!Mock.LastResult) mockLastResult();
     let columnsArray = Test.utils.data.sheet[4];
     let row = RowFactory.create();
     row.originalColumnsArray = columnsArray;
@@ -183,5 +469,17 @@ describe('TemplateService service', function () {
     row.countRowsAfterLastResult = Mock.LastResult ? Mock.LastResult.countRowsAfterLastResult + 1 : 0;
     service.fillRow(row, columnsArray, Mock.Template);
     Mock.RowFilled = row;
+  }
+  
+  function mockRowCreated() {
+    if (!Mock.Template) mockTemplate();
+    if (!Mock.LastResult) mockLastResult();
+    Mock.RowCreated = service.createRow(
+      Mock.Template,
+      Test.utils.data.sheet[4],
+      Mock.LastResult,
+      Mock.LastResult,
+      4
+    );
   }
 });
